29862aa46617c82a4895b9d6e760bd43443c6316
ysevinchoy2197 <ysevinchoy2197@gmail.com>
2025-10-31 17:55:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+# 1
+counter = 0
+for i in range(1,11):
+    print(i)
+
+
+
+# 2
+counter = 0
+for i in range(10, 1):
+    counter -= 1
+    print(i)
+
+
+
+
+# 3
+n = int(input('n sonini kirit: '))
+
+for i in range(1, n):
+    if i % 2 == 0:
+        print(i)
+
+
+
+# 4
+n = int(input('n sonini kirit: '))
+
+for i in range(1, n):
+    if i % 1 == 0:
+        print(i)
+
+
+
+
+#5
+n = int(input('n sonini kirit: '))
+
+for i in range(1, n):
+    if i + 3 == 0:
+ 
